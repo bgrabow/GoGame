5c658c9a624026496c85276f91872106bc25edea
Ben Grabow <ben.grabow@gmail.com>
2016-07-15 13:32:22 -0500

    Removed Shoes-based prototype. Added Tk-based prototype with working callbacks.

--- lib/GoBoardDisplay.rb
+++ lib/GoBoardDisplay.rb
@@ -1,5 +1,26 @@
+require 'tk'
+
 class GoBoardDisplay
-  def initialize(board)
-    
+  def initialize(context)
+    @context = context
+    @root = TkRoot.new do
+      title "Go"
+      minsize(500, 400)
+    end
+
+    @label = TkLabel.new(@root) { pack }
+    @label.configure('text' => 'Hello world')
+
+    @button = TkButton.new(@root) { pack }
+    @button.configure('text' => 'Alert')
+    @button.command { context.alert }
+  end
+
+  def change_label
+    @label.configure('text' => 'Goodbye world')
+  end
+
+  def mainloop
+    Tk.mainloop
   end
 end

27f501070cbbdc03ebedb2eddfc2a4d0fc20053e
Ben Grabow <ben.grabow@gmail.com>
2016-07-15 13:34:28 -0500

    Removed tests that didn't test any meaningful behavior.

--- spec/GoBoardDisplay_spec.rb
+++ spec/GoBoardDisplay_spec.rb
@@ -1,13 +1,6 @@
 require_relative '../lib/GoBoardDisplay'
 
 describe "GoBoardDisplay" do
-  it "can be instantiated" do
-    expect {display = GoBoardDisplay.new(Object.new)}.not_to raise_error
-  end
-  
-  it "accepts a GoBoard init param" do
-    expect {display = GoBoardDisplay.new(Object.new)}.not_to raise_error
-  end
   
   
 end
